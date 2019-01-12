31a341f670d5d3766dfdfaef29739b5b66764f04
Manuel Tejada <manute32@gmail.com>
2019-01-12 16:34:18 +0100

    Beer list component

---
+++ frontend/src/app/beer-list/beer-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { BeerService } from '../services/beer.service';
+
+const abvs = [
+  { label: 'Less than 4%', value: '-3' },
+  { label: '4% - 6%', value: '4,6' },
+  { label: '7% - 9%', value: '7,9' },
+  { label: '10% - 12%', value: '10,12' },
+  { label: 'More than 12%', value: '+12' },
+];
+
+const ibus = [
+  { label: 'Light', value: '0,10' },
+  { label: 'A Little Bitter', value: '11,20' },
+  { label: 'Average', value: '20,50' },
+  { label: 'Bitter', value: '51,80' },
+  { label: 'Very Bitter', value: '+81' },
+];
+
+@Component({
+  selector: 'app-beer-list',
+  templateUrl: './beer-list.component.html',
+  styleUrls: ['./beer-list.component.scss']
+})
+export class BeerListComponent implements OnInit {
+  beers: Array<any>;
+  abvs = abvs;
+  ibus = ibus;
+  selectedAbv = '+1';
+  selectedIbu = '+1';
+  filters;
+
+  constructor(private beerService: BeerService) { }
+
+  ngOnInit() {
+    this.beerService.getBeers(this.filters)
+      .subscribe(data => {
+        this.beers = data.data;
+      });
+  }
+
+  refetchBeers() {
+    this.filters = {
+      abv: this.selectedAbv,
+      ibu: this.selectedIbu,
+    };
+
+    this.beerService.getBeers(this.filters)
+      .subscribe(data => this.beers = data.data);
+  }
+
+}
