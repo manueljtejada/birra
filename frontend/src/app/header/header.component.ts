afc4f532b7ead7096f1ce5b9e2b5c42ad7f0da53
Manuel Tejada <manute32@gmail.com>
2019-01-13 23:48:47 +0100

    Only show login button when user is not logged in

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { LoginComponent } from '../login/login.component';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,14 @@ import { LoginComponent } from '../login/login.component';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public user;
+  public userLoggedIn: boolean;
+  public login;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, public auth: LoginService) { }
 
   ngOnInit() {
+    this.auth.getUser().subscribe(user => this.user = user);
   }
 
   openLoginDialog() {
@@ -21,6 +26,15 @@ export class HeaderComponent implements OnInit {
     };
 
     this.dialog.open(LoginComponent, modalOptions);
+  }
+
+  doLogout() {
+    this.auth.doLogout()
+      .subscribe();
+  }
+
+  @HostListener('window:storage', ['$event']) process() {
+    this.auth.getUser().subscribe(login => this.login = login);
   }
 
 }
--- frontend/src/app/services/login.service.ts
+++ frontend/src/app/services/login.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import * as firebase from 'firebase/app';
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, Subscriber, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+interface User {
+  uid: string;
+  email: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.user = afAuth.authState;
+  }
 
   doRegister(value) {
     return new Promise<any>((resolve, reject) => {
@@ -25,5 +35,13 @@ export class LoginService {
         .then(res => resolve(res))
         .catch(err => reject(err));
     });
+  }
+
+  doLogout() {
+    return of(this.afAuth.auth.signOut());
+  }
+
+  getUser(): Observable<any> {
+    return this.user;
   }
 }
